c66a64a2fe15c0078b4183764105cb7f418e41cc
Prayani <prayani.singh0106@gmail.com>
2019-04-11 21:52:16 -0400

    Staff

---
+++ src/com/dbms/wolfline/Staff.java
@@ -0,0 +1,89 @@
+package com.dbms.wolfline;
+
+public class Staff {
+
+	private int id;
+	private String name;
+	private int age;
+	private String gender;
+	private String jobTitle;
+	private String professionalTitle;
+	private String department;
+	private String phone;
+	private String address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public String getProfessionalTitle() {
+		return professionalTitle;
+	}
+
+	public void setProfessionalTitle(String professionalTitle) {
+		this.professionalTitle = professionalTitle;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	
+
+}
--- src/com/dbms/wolfline/Ward.java
+++ src/com/dbms/wolfline/Ward.java
@@ -9,39 +9,42 @@ public class Ward {
 	 * int(11) | NO | | NULL | | | responsible_nurse | int(11) | YES | MUL | NULL |
 	 * |
 	 */
-	
-	 private String wardNo;
-	 private String capacity;
-	 private String cost;
-	 private String responsible_nurse;
-	 
-	public String getWardNo() {
+
+	private int wardNo;
+	private int capacity;
+	private int cost;
+	private int responsible_nurse;
+
+	public int getWardNo() {
 		return wardNo;
 	}
-	public void setWardNo(String wardNo) {
+
+	public void setWardNo(int wardNo) {
 		this.wardNo = wardNo;
 	}
-	public String getCapacity() {
+
+	public int getCapacity() {
 		return capacity;
 	}
-	public void setCapacity(String capacity) {
+
+	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
-	public String getCost() {
+
+	public int getCost() {
 		return cost;
 	}
-	public void setCost(String cost) {
+
+	public void setCost(int cost) {
 		this.cost = cost;
 	}
-	public String getResponsible_nurse() {
+
+	public int getResponsible_nurse() {
 		return responsible_nurse;
 	}
-	public void setResponsible_nurse(String responsible_nurse) {
+
+	public void setResponsible_nurse(int responsible_nurse) {
 		this.responsible_nurse = responsible_nurse;
 	}
-	 
-	 
-	
-	 
-	 
+
 }
